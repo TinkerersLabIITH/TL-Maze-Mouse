f752186d3d06fe900d45bbddb4e6069b15ccc213
MohamedKaif-ft <141742064+MohamedKaif-ft@users.noreply.github.com>
2023-09-07 00:43:22 +0530

    LeaderBoardAdded

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,9 +1,23 @@
-function LeaderBoard() {
+import "../index.css"
+import tinkererLogo from "../assets/tinkererlogo.svg"
+import micromouseLogo from  "../assets/maze.svg"
+import milanlogo from "../assets/logocream.png"
+// import gifLogo from "../assets/logoGif.mp4"
+function LandingPage() {
     return(
-        <div className="leaderboard">
-            LeaderBoard
+        <div className="landingPage">
+            <div className="micromouseLogo">
+                <img src={micromouseLogo} alt ="The logo of MicroMouse"/>
+                </div>
+                <div className="playpage-logo">
+          <img src={milanlogo} alt="milan logo" />
+          <img src={tinkererLogo} alt="tinkerer logo" />
+        </div>
+            <div className="lpBox">
+               <p className="headingMM">LEADER BOARD</p>
+            </div>
         </div>
     )
 }
 
-export default LeaderBoard;
+export default LandingPage;
