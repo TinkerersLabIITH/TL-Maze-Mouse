4eb75ff8702352554e9c9a2440c2d8b301c83b18
MohamedKaif-ft <141742064+MohamedKaif-ft@users.noreply.github.com>
2023-09-14 01:16:05 +0530

    instruction

--- src/pages/Instructions.js
+++ src/pages/Instructions.js
@@ -3,38 +3,19 @@ import { useNavigate, useLocation } from "react-router-dom"
 import {useState} from "react"
 import tinkererLogo from "../assets/tinkererlogo.svg"
 import micromouseLogo from "../assets/maze.svg"
-import milanlogo from "../assets/logocream.png"
 import mmouse from "../assets/micromouselogo.png"
-import axios from "axios"
+
 // import gifLogo from "../assets/logoGif.mp4"
 function Instructions() {
   const location = useLocation()
   const navigate = useNavigate();
   const userEmail = location.state ? location.state.userEmail : null
-  const [isRequestSent, setIsRequestSent] = useState(false);
-  const handleButtonClick = () => {
-    if (userEmail && !isRequestSent) {
-      
-      const postUrl = "https://tinkererslabiith.github.io/micromouse-game/";
-
-      
-      const postData = {
-        userEmail: userEmail,
-      };
-      axios
-      .post(postUrl, postData)
-      .then((response) => {
-        console.log("POST request successful", response);
-        setIsRequestSent(true);
-        <a href="https://tinkererslabiith.github.io/micromouse-game/"/>
-      })
-      .catch((error) => {
-        console.error("Error sending POST request", error);})
- 
-  console.log(userEmail)
-}}
- 
-  
+  const handleContinueClick = () => {
+    if (userEmail) {
+      const targetUrl = `https://tinkererslabiith.github.io/micromouse-game/?userEmail=${encodeURIComponent(userEmail)}`;
+      window.location.href = targetUrl;
+    }
+  };
   return (
     <div className="landingPage">
       <div className="micromouseLogo">
@@ -51,7 +32,7 @@ function Instructions() {
         <p className="paraMMIpSmall">loremipsum</p>
        
         <div className="playpage-buttons" style={{ marginTop: "10vh" }}>
-          <div className="pink-button" onClick={handleButtonClick}>
+          <div className="pink-button" onClick={handleContinueClick}>
             <div className="pink-button-inner" style={{ width: "90%" }}>
               <div className="pink-button-rect" />
               <p style={{ fontSize: "1.5rem", width: "max-content" }}>
