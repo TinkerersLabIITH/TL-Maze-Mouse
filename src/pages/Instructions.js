311f02ecd87f548a29a76894f50fd3b1d4f8e730
MohamedKaif-ft <141742064+MohamedKaif-ft@users.noreply.github.com>
2023-09-13 23:26:15 +0530

    committed

--- src/pages/Instructions.js
+++ src/pages/Instructions.js
@@ -1,19 +1,44 @@
 import "../kaif.css"
 import { useNavigate, useLocation } from "react-router-dom"
+import {useState} from "react"
 import tinkererLogo from "../assets/tinkererlogo.svg"
 import micromouseLogo from "../assets/maze.svg"
 import milanlogo from "../assets/logocream.png"
 import mmouse from "../assets/micromouselogo.png"
+import axios from "axios"
 // import gifLogo from "../assets/logoGif.mp4"
 function Instructions() {
   const location = useLocation()
-  const userEmail = location.state ? location.state.userEmail : null
-  console.log(userEmail)
   const navigate = useNavigate();
+  const userEmail = location.state ? location.state.userEmail : null
+  const [isRequestSent, setIsRequestSent] = useState(false);
+  const handleButtonClick = () => {
+    if (userEmail && !isRequestSent) {
+      
+      const postUrl = "https://tinkererslabiith.github.io/micromouse-game/";
+
+      
+      const postData = {
+        userEmail: userEmail,
+      };
+      axios
+      .post(postUrl, postData)
+      .then((response) => {
+        console.log("POST request successful", response);
+        setIsRequestSent(true);
+        <a href="https://tinkererslabiith.github.io/micromouse-game/"/>
+      })
+      .catch((error) => {
+        console.error("Error sending POST request", error);})
+ 
+  console.log(userEmail)
+}}
+ 
+  
   return (
     <div className="landingPage">
       <div className="micromouseLogo">
-        <img src={micromouseLogo} alt="The logo of MicroMouse" />
+        <img src={micromouseLogo} alt="The logo of MicroMouse"/>
       </div>
       <div className="playpage-logo">
         <img src={tinkererLogo} alt="tinkerer logo" />
@@ -24,8 +49,9 @@ function Instructions() {
         <p className="headingMMIp">HI PLAYER!!</p>
         <p className="paraMMIp">INSTRUCTIONS</p>
         <p className="paraMMIpSmall">loremipsum</p>
+       
         <div className="playpage-buttons" style={{ marginTop: "10vh" }}>
-          <div className="pink-button" onClick={() => navigate('/game')}>
+          <div className="pink-button" onClick={handleButtonClick}>
             <div className="pink-button-inner" style={{ width: "90%" }}>
               <div className="pink-button-rect" />
               <p style={{ fontSize: "1.5rem", width: "max-content" }}>
@@ -34,6 +60,7 @@ function Instructions() {
             </div>
           </div>
         </div>
+       
       </div>
     </div>
   )
