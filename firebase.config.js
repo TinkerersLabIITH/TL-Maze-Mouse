57d58b56258e177088e502ea4d6f5d10c8adc5dd
Himanshu Sekhar <hsmaharaj1@gmail.com>
2023-09-16 03:48:54 +0530

    changed database for testing

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9R03gG9h-81PNuUjuLSLcUoAfXdvSKqg",
+  authDomain: "micromouse-cfd0a.firebaseapp.com",
+  projectId: "micromouse-cfd0a",
+  storageBucket: "micromouse-cfd0a.appspot.com",
+  messagingSenderId: "90603999982",
+  appId: "1:90603999982:web:9e135261c8ce3a819659ff"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
