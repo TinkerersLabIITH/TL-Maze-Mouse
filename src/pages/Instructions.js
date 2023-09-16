d4b28fc3113a823e9fc1f84bfecf011623a0800d
Suddhodhan <suddhu040804@gmail.com>
2023-09-16 15:56:39 +0530

    Om

--- src/pages/Instructions.js
+++ src/pages/Instructions.js
@@ -53,8 +53,10 @@ function Instructions() {
         {/* <video width="320" height="240" src={gifLogo} loop="true" autoplay="true"/> */}
         <p className="headingMMIp">HI PLAYER!!</p>
         <p className="paraMMIp">INSTRUCTIONS</p>
-        <p className="paraMMIpSmall">loremipsum</p>
-
+        <p className="paraMMIpSmall">1. Just swipe on the screen in the direction you want to move the mouse</p>
+        <p className="paraMMIpSmall">2. Maximum Time is 5 Minutes</p>
+        <p className="paraMMIpSmall">3. Score will be calculated based on the time taken for garbbing the cheese.</p>
+        
         <div className="playpage-buttons" style={{ marginTop: "10vh" }}>
           <div className="pink-button" onClick={handleContinueClick}>
             <div className="pink-button-inner" style={{ width: "90%" }}>
